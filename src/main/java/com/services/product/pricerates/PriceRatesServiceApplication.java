e750cb91e060f9154cddf27f4feb799f2a71a7c6
Ruth RMan <ruth.rml@outlook.es>
2025-01-16 19:37:58 +0100

    Aplicacion base.

---
+++ src/main/java/com/services/product/pricerates/PriceRatesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.services.product.pricerates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceRatesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceRatesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/services/product/price_rates_service/PriceRatesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.product.price_rates_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceRatesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
